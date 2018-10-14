dcb1ab59a1b7fae50602bb0a5aabf7b4c14de310
Theodore <2226390@gmail.com>
2018-10-14 21:43:55 +0300

    add title add ThemeProvider

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,17 +1,26 @@
 import { Component } from "react";
-import Styles from "../Styles";
+import { injectGlobal } from "styled-components";
+import { ThemeProvider, globalStyle } from "@smooth-ui/core-sc";
+import reset from "styled-reset";
+import Head from "next/head";
+
+import { main } from "../Styles";
 import Fonts from "../Fonts";
 import Header from "../Header";
+
+injectGlobal`${reset} ${globalStyle()}`;
 
 export default class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
       <div>
-        <Styles />
+        <Head>
+          <title>Theodore Chernin</title>
+        </Head>
         <Fonts />
         <Header />
-        {children}
+        <ThemeProvider theme={main}>{children}</ThemeProvider>
       </div>
     );
   }
--- src/components/common/Styles/index.js
+++ src/components/common/Styles/index.js
@@ -1,13 +1,8 @@
-import styledNormalize from "styled-normalize";
-import { injectGlobal } from "styled-components";
+import { theme } from "@smooth-ui/core-sc";
 
-injectGlobal`
-  ${styledNormalize}
+const main = {
+  ...theme,
+  headingsFontFamily: 'Lato', 
+};
 
-  body{
-    font-family: Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background-color: hsl(0, 0%, 99%);
-  }
-`;
-
-export default () => null;
+export { main };
